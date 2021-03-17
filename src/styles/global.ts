a285f9d87ab148f79b14c1b5b1f5de5bd620ac2f
Emerson Melo Martins <emerson25xd@gmail.com>
2021-03-17 11:28:06 -0300

    Adição de styled-components e estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-size: 16px -> tamanho padrão para desktop
+  // 93.75% -> Diminui 1 pixel do tamanho padrão (16 - 0.9375) = 15
+  // 87.5% -> Diminui 1 pixel do tamanho acima (15 - 0.875) = 14
+
+  // Utilizar porcentagem ao invés de px, pois se for mobile ou tablet, o font-size pode ser
+  // diferente de 16px
+  // REM -> 1rem = font-size da pagina (16px)
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+
+`;
